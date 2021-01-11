9f85ae94cf485ff7e84c8678d29fc49279ed3216
valhuber <valjhuber@gmail.com>
2021-01-10 17:00:44 -0800

    initial commit

---
+++ database/models/__init__.py
@@ -0,0 +1,1 @@
+from database.models.book import Book
---
+++ database/models/user.py
@@ -0,0 +1,17 @@
+from typing import List
+from safrs import SAFRSBase
+from db import db
+from database.models.types.email_type import EmailType
+
+
+class User(SAFRSBase, db.Model):
+    """
+        description: User description
+    """
+    #  db_commit = False;
+    __tablename__ = "Users"
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, default="")
+    # email = db.Column(db.String, default="")
+    email = db.Column(EmailType, default="")
+    books: List = db.relationship("Book", back_populates="user", lazy="dynamic")
