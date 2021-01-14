dd61c4b595bb65838cbe7164fbec94b638e1e872
valhuber <valjhuber@gmail.com>
2021-01-14 11:02:57 -0800

    temp kludge to set SQLALCHEMY_DATABASE_URI

--- api/expose_api_models.py
+++ api/expose_api_models.py
@@ -3,7 +3,3 @@ from safrs import SAFRSAPI
 from api.json_encoder import SAFRSJSONEncoderExt
 from database import models
 
-
-def expose_models(app, HOST="localhost", PORT=5000, API_PREFIX="/api"):
-    """ generate api.expose_object(<table>)
-    """
