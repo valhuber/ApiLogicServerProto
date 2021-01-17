e9046d67fba595089fd6c60921a62ac3b2c05757
valhuber <valjhuber@gmail.com>
2021-01-17 12:31:42 -0800

    investigate app module -> api module

---
+++ api/__init__.py
@@ -0,0 +1,70 @@
+import safrs
+from logic_bank.logic_bank import LogicBank
+from logic_bank.exec_row_logic.logic_row import LogicRow
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+
+from admin.admin_view_ext import AdminViewExt
+from api import expose_api_models
+from database import db  # , session
+from flask import Flask
+from api.json_encoder import SAFRSJSONEncoderExt
+from logic import rules_bank
+from safrs import SAFRSAPI, ValidationError
+from database import models
+
+try:
+    from flask_admin import Admin
+    from flask_admin.contrib import sqla
+except:
+    print("Failed to import flask-admin")
+
+
+def create_app(config_filename=None, host="localhost"):
+    app = Flask("API Logic Server")
+    app.config.from_object("config.Config")
+    #    app.config.update(SQLALCHEMY_DATABASE_URI="sqlite://")
+    # from database import db  # , session  FIXME eh?
+    use_file = False
+    if use_file:  # FIXME Achim cleanup this is a little obscure - can we bring inline?
+        db.db.init_app(app)
+        session = db.session
+    else:
+        # db: SQLAlchemy = SQLAlchemy()  REMOVE
+        db = safrs.DB  # opens (what?) database, returning session
+        Base: declarative_base = db.Model
+        session: Session = db.session
+        print("app/__init__#create_app - got session: " + str(session))
+
+    def constraint_handler(message: str, constraint: object,
+                           logic_row: LogicRow):  # message: str, constr: constraint, row: logic_row):
+        raise ValidationError(message)
+
+    LogicBank.activate(session=session, activator=rules_bank.declare_logic, constraint_event=constraint_handler)
+
+    with app.app_context():
+        db.init_app(app)
+        # create_api(app, host)   REMOVE
+        expose_api_models.expose_models(app, host)
+        # FIXME required?  does not work - create_admin_ui(app)
+
+    return app
+
+
+def create_admin_ui(app):
+    try:
+        admin = Admin(app, url="/admin")
+        for model in [models.User, models.Book, models.StoreModel, models.ItemModel]:
+            #  admin.add_view(sqla.ModelView(model, db.session))
+            admin.add_view(AdminViewExt(model, db.session))
+    except Exception as exc:
+        print(f"Failed to add flask-admin view {exc}")
+
+
+def create_app_for_test(config_filename=None, host="localhost"):
+    app = Flask("LogicBank Demo App")
+    app.config.from_object("config.Config")
+    db.init_app(app)
+    #  https://flask-sqlalchemy.palletsprojects.com/en/2.x/contexts/
+    app.app_context().push()
+    return app
--- api_logic_server_run.py
+++ api_logic_server_run.py
@@ -17,7 +17,11 @@ import logic_bank_utils.util as logic_bank_utils
 from flask import render_template
 from safrs import ValidationError
 
-import app as app  # database opened here
+use_api_module = True  # FIXME - review 7 choose
+if use_api_module:
+    import api as app  # remove app folder
+else:
+    import app as app  # database opened here
 
 
 # address where the api will be hosted, change this if you're not running the app on localhost!
