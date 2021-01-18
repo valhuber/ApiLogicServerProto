b031b938a4e19c9d5f5faa716bedb2143739ed4c
valhuber <valjhuber@gmail.com>
2021-01-18 08:22:23 -0800

    set up logging

--- api/__init__.py
+++ api/__init__.py
@@ -20,7 +20,32 @@ except:
     print("Failed to import flask-admin")
 
 
+def setup_logging():
+    # Initialize Logging
+    import logging
+    import sys
+
+    logic_logger = logging.getLogger('logic_logger')  # for debugging user logic
+    logic_logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(message)s - %(asctime)s - %(name)s - %(levelname)s')
+    handler.setFormatter(formatter)
+    logic_logger.addHandler(handler)
+
+    do_engine_logging = False
+    engine_logger = logging.getLogger('engine_logger')  # for internals
+    if do_engine_logging:
+        engine_logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(message)s - %(asctime)s - %(name)s - %(levelname)s')
+        handler.setFormatter(formatter)
+        engine_logger.addHandler(handler)
+
+
 def create_app(config_filename=None, host="localhost"):
+    setup_logging()
     app = Flask("API Logic Server")
     app.config.from_object("config.Config")
     #    app.config.update(SQLALCHEMY_DATABASE_URI="sqlite://")
