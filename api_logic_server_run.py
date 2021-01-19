7fc1cf3e280dc60c83cc30c9f1e4a18bbcf2a719
valhuber <valjhuber@gmail.com>
2021-01-19 07:40:13 -0800

    invoke expose_existing as function (not cmd)

--- api_logic_server_run.py
+++ api_logic_server_run.py
@@ -17,7 +17,7 @@ import logic_bank_utils.util as logic_bank_utils
 from flask import render_template
 from safrs import ValidationError
 
-import app as app  # database opened here
+import api as app  # database opened here
 
 # address where the api will be hosted, change this if you're not running the app on localhost!
 host = sys.argv[1] if sys.argv[
